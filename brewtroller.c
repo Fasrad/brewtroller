db7ed3e0def395c52f8bd83d4ab43b8933f380e7
Fasrad <millerchaz@gmail.com>
2013-03-11 21:39:01 -0500

    tested and stable. Brewed 1 batch so far. A buzzer would be kinda neat.

--- brewtroller.c
+++ brewtroller.c
@@ -3,17 +3,18 @@ brewtroller.c
 
 a simple temperature/boil controller for brewing or sous vide
 
-It has 2 knobs, one temperature setpoint and one duty cycle %
-With no temp probe connected, it acts as an open-loop 0-100% boil controller
+by chaz miller 2013
+
+Controller has 2 knobs, temperature setpoint and duty cycle.
+With no temp probe, it acts as an open-loop 0-100% boil controller.
 With temp probe connected it acts as a simple heat-only thermostat.
-% duty cycle adjustment is still applied even in temp control mode. 
+The duty cycle adjustment is still applied even in temp control mode. 
 
 HARDWARE:
 for ATMEGAxx8 set at 1MHz. 
 LM335 on PC0 (10mV/K absolute)
-3-4V pot on PC1 (temperature setpoint)
-0-5V pot on PC2 (duty cycle control)
-AREF=5V
+3-4V pot on PC1 (temperature setpoint knob)
+0-5V pot on PC2 (duty cycle control knob)
 SSR and LED connected to PBwhatever
 
 This is free software: you can redistribute it and/or modify
@@ -23,10 +24,10 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 ******************************************************************/
 
+//todo: hardware PWM, oversampling+voltage mapping for sensor
 #include <avr/io.h>
 
 void delay(uint16_t);
-void die (uint8_t);
 void blink (uint8_t);
 void adc_init(void);
 void adc_read(uint8_t);
@@ -44,48 +45,36 @@ int main(){
     //8 bit Timer 0 is used by delay().
     TCCR0A = 0;                //stardard timer mode (page 103)
     TCCR0B = 2;                //fcpu / 1
-    for (int i=0; i<8; i++){   //startup blinkenled for user
-	PORTB |= (1<<5);
-	delay(80);            
-	PORTB &= ~(1<<5);
-	delay(80);
-    }
     adc_init();
-    delay(800);
     /****************************************
     *****main loop***************************
     ****************************************/
-    blink(3); //countdown for user convenience
-    while(1){  
-	adc_read(0);probe_ad = ADCW;      //read temp probe
-	if(probe_ad < 900){               //read setpoint knob if probe exists
+    blink(1); 
+    for(;;){  
+	adc_read(0);probe_ad = ADCW;    //read temp probe
+	if(probe_ad < 900){             //read setpoint knob iff probe exists
+	    //probe_ad = probe_ad-612;  //map 3-3.75V to 0-5V for direct
+	    //probe_ad = probe_ad<<3;   //comparison with setpoint knob
 	    adc_read(1);                
-	    if(ADCW > (probe_ad + hyst)){         //thermostat
+	    if(ADCW > (probe_ad + hyst)){   //thermostat block
 		write_pwm(1);
 	    }else if(ADCW < (probe_ad-hyst)){
 		write_pwm(0);
 	    }
-	}else{                  //just apply duty cycle setting if no probe
+	}else{                //just apply duty cycle setting if no probe
 	    write_pwm(1);
 	}
 	PORTB = 0xFF;
-	delay(PWM_duty);            //200ms
+	delay(PWM_duty); 
 	PORTB = 0;
 	delay(255-PWM_duty);
     } //infty
 }//main
-
 void delay(uint16_t me){    //at 1MHz, each unit is 2.55us. 1ms is 4units. 
     while(me){
 	while(TCNT0 < 128){}
 	me--;
 	while(TCNT0 > 128){}
-    }
-}
-void die (uint8_t me){
-    while(1){
-	blink(me);
-	delay(2000);
     }
 }
 void blink (uint8_t me){

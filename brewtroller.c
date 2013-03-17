2d7a0dd96f6f4b4107b4b13fed2d7665d2185899
Fasrad <millerchaz@gmail.com>
2013-03-17 01:41:22 -0500

    commit after writing doc

--- brewtroller.c
+++ brewtroller.c
@@ -5,19 +5,22 @@ a simple temperature/boil controller for brewing or sous vide
 
 by chaz miller 
 
-Controller has 2 knobs, temperature setpoint and duty cycle.
-Temp probe presence is auto-detected.
 With no temp probe, it acts as an open-loop 0-100% boil controller.
+If installed, boil override switch allows fixed boil power to be set. 
+Temp probe presence is auto-detected.
 With temp probe connected it acts as a simple heat-only thermostat.
-The duty cycle adjustment is still applied even in temp control mode. 
+The duty cycle knob setting is still applied even in temp control mode. 
 
 HARDWARE:
 for ATMEGAxx8 set at 1MHz running at 5V. 
-LM335 diode temp sensor on PC0 (10mV/K absolute)
-Pot on PC1 (temperature setpoint knob)
-Pot on PC2 (duty cycle control knob)
-SSR connected to PB3 (OC2A) for heating element
-LED connected to PB5 (optional)
+SSR connected to PB2 (OC1B) for heating element
+SPDT or 2 SPST switch connected from PB4 and PB5 to ground (optional, simplest)
+Pot on PC2 (duty cycle control knob, optional)
+LED connected to PB5 (recommended)
+NO contactor connected to PB6 (optional)
+LM335 diode temp sensor + 10kish resistor on PC0 (10mV/K absolute) (optional)
+Pot on PC3 (temp setpoint knob) (needed if temp sensor is used)
+
 For a bit more real precision divide down Vcc and run the pots and AREF at 4V.
 Bracket temp setpoint knob with resistors to taste.
 
@@ -27,49 +30,61 @@ version. This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 ******************************************************************/
+//todo: add NO contactor output, add GFCI?
 
 #include <avr/io.h>
+
+#define boil_setpoint 0x980    //adjust to your system
+#define hyst 2    //about 1/10th deg. C per count at 12 bits; optional
 
 void delay(uint16_t);
 void blink (uint8_t);
 void adc_init(void);
 uint16_t adc_read(uint8_t);
-void write_pwm(uint8_t);
 
-uint8_t hyst = 2;    //about 1/10th deg. C per count at 12 bits 
 uint16_t probe_ad;  
 uint16_t set_ad;  
 
 int main(){
-    DDRB = 0xFF;
-    DDRD = 0xFF;
     //8 bit Timer 0 is used by delay().
-    TCCR0A = 0;                //stardard timer mode (page 103)
+    TCCR0A = 0;                //standard timer mode (page 103)
     TCCR0B = 2;                //fcpu / 1
     //16-bit Timer 1 used as output PWM on OC1B PB2 (Arduino pin 10) p.115
-    //noninverting phase correct, OCR1A=TOP mode channel B p135 
+    //noninverting phase correct, CTC-PWM hybrid mode p135 
     TCCR1A = (1<<COM1B1)|(1<<WGM11)|(1<<WGM10); 
     TCCR1B = (1<<WGM13)|(1<<CS12);                 //  clk/256
-    //TCCR1B = (1<<WGM13)|(1<<CS10);                 //  clk/1
     //TCCR1B = (1<<WGM13)|(1<<CS11)|(1<<CS10);       //  clk/64
-    OCR1A = 0x1000;             //set TOP=12 bits to match ADC word size
-    OCR1B = 100;
+    //TCCR1B = (1<<WGM13)|(1<<CS10);                 //  clk/1
+    OCR1A = 0x0FF0;             //sets pwm TOP 
+    OCR1B = 0;
+    PORTB = 0xFF;
+    DDRB = 0b000100100;        //LED on PB5; OC1B is PB2
     adc_init();
     /****************************************
     *****main loop***************************
     ****************************************/
     blink(3);
     for(;;){  
-	probe_ad = adc_read(0); 
-	if(probe_ad < 3500){       //read setpoint knob iff probe detected 
-	    set_ad = adc_read(1);                
-	    if(set_ad > (probe_ad)){   //thermostat block
-		OCR1B = adc_read(2);
-	    }else if(set_ad <= (probe_ad)){
-		OCR1B = 0;
+	if(PORTB&1<<4){                      //if switch is not 'off'
+	    PORTB|=1<<5;                     //turn on contactor
+	    if(PORTB&1<<3){                  //if switch is not 'boil'
+		probe_ad = adc_read(0); 
+		if(probe_ad < 0x0EFF){       //continue iff probe detected 
+		    set_ad = adc_read(1);                
+		    if(set_ad > (probe_ad + hyst)){   //thermostat block
+			OCR1B = adc_read(2) & 0xFF00;
+		    }else if(set_ad < (probe_ad - hyst)){
+			OCR1B = 0x0000;
+		    }
+		}else{                       //no probe; no boil override
+		    OCR1B = adc_read(2) & 0xFF00;
+		}
+	    }else{                           //'boil' switch is on
+		OCR1B = boil_setpoint;
 	    }
-	}else{                //just apply duty cycle setting if no probe
-	    OCR1B = adc_read(2) & 0xFF00;
+	}else{
+	    PORTB&=~(1<<5);       //kill everything if switch is off
+	    OCR1B = 0x0000;
 	}
    } //infty
 }//main
@@ -78,9 +93,9 @@ void adc_init(void){
     ADMUX |= (1<<REFS0);           //Avcc (5v)
     //ADMUX |= (1<<ADLAR);           //left align for 8-bit operation
     ADCSRA |= (1<<ADEN); 
-    ADCSRA |= (1<<ADSC);  
+    //ADCSRA |= (1<<ADSC);  
 }
-uint16_t adc_read(uint8_t me){
+uint16_t adc_read(uint8_t me){    //expects register value, not port pin label
     uint16_t ad_bucket=0;
     ADMUX &= 0xF0;
     ADMUX |= me;

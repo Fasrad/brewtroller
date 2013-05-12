8ab5b71d517b978ab2783dd0bf8dadea0d049ecb
Fasrad <millerchaz@gmail.com>
2013-05-11 21:43:23 -0500

    extra comments and updated docs

--- brewtroller.c
+++ brewtroller.c
@@ -34,6 +34,7 @@ int main(){
     TCCR1A = (1<<COM1B1)|(1<<WGM11)|(1<<WGM10); 
     OCR1A = 0x0FF0;             //sets pwm TOP 
     OCR1B = 0;
+    //user select pwm speed
     switch ((PINB&0xC0)>>6){
 	case 0:                                            //  PB6=PB7=gnd
 	    TCCR1B = (1<<WGM13)|(1<<CS10);                 //  clk/1; 240kHz
@@ -51,22 +52,22 @@ int main(){
     //8 bit Timer 2 for compressor delay; 1 may work?
     TCCR2B = (1<<CS22)|(1<<CS21)|(1<<CS20);                //CPU/1024; 2Hz
     PORTB = 0xFF;
-    PORTB|=1<<5;                     //turn on LED
     DDRB = 0b000100100;        //LED on PB5; OC1B is PB2
+    PORTB|=1<<5;                     //turn on LED
     adc_init();
     if (PINB&1<<0){dly=0x200;}   //enable compressor delay; 2 cnts=1sec
     /****************************************
     *****main loop***************************
     ****************************************/
     for(;;){  
-	if(TIFR2&1<<TOV2){dlycnt++;TIFR2|=1;} //increment compressor delay
+	if(TIFR2&1<<TOV2){dlycnt++;TIFR2|=1;} //incr comp. delay if overflow
 	if(PINB&1<<3){                      //if switch is not 'WFO'
 	    if(PINB&1<<4){                  //if switch is not 'boil'
 		probe_ad = adc_read(0);     //read probe
 		if(probe_ad < 0x0EFF){       //continue iff probe detected 
 		    set_ad = adc_read(1);    //be thermostat 
 		    if((set_ad>(probe_ad+hyst))&&(dlycnt>dly)){  
-			OCR1B = adc_read(2) & 0xFF00;
+			OCR1B = adc_read(2) & 0xFF00; //still apply duty cycle
 			dlycnt=0;
 		    }else if(set_ad < (probe_ad - hyst)){
 			OCR1B = 0x0000;
@@ -89,7 +90,7 @@ void adc_init(void){
     //ADMUX |= (1<<ADLAR);           //left align for 8-bit operation
     ADCSRA |= (1<<ADEN); 
 }
-uint16_t adc_read(uint8_t me){    //expects register value, not port pin label
+uint16_t adc_read(uint8_t me){    //expects ADMUX register value
     uint16_t ad_bucket=0;
     ADMUX &= 0xF0;
     ADMUX |= me;
